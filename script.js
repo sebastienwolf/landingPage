ff069291a6b9649c1a6a3dfb112ed95cc4518823
sebastienwolf <s.mouret.pro@gmail.com>
2022-02-02 22:23:25 +0100

    form header and scroll

--- script.js
+++ script.js
@@ -12,11 +12,4 @@ $(window).scroll(function() {
     $(window).scrollTop(0); //cheating
   });
 
-
-  // animation caroussel
-  var angle = 0;
-  function galleryspin(sign) { 
-  spinner = document.querySelector("#spinner");
-  if (!sign) { angle = angle + 45; } else { angle = angle - 45; }
-  spinner.setAttribute("style","-webkit-transform: rotateY("+ angle +"deg); -moz-transform: rotateY("+ angle +"deg); transform: rotateY("+ angle +"deg);");
-  }
+  // ==========================
